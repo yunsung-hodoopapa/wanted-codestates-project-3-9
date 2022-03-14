7285202a299edd27f52435955c826cac57d5b996
khw970421 <khw970421@kakao.com>
2022-03-14 16:36:22 +0900

    🎨 chore : layout 설정

--- src/components/Grade.jsx
+++ src/components/Grade.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BsStarFill, BsStar } from 'react-icons/bs';
-// import ReviewTitle from './ReviewTitle';
-// import styled from 'styled-components';
+import ReviewTitle from './ReviewTitle';
+import styled from 'styled-components';
 const Grade = () => {
   const [clicked, setClicked] = useState([false, false, false, false, false]);
 
@@ -19,50 +19,60 @@ const Grade = () => {
   console.log(clicked);
 
   return (
-    <div>
-      <p>Rating</p>
-      <div>
-        {clicked[0] ? (
-          <BsStarFill onClick={e => handleStarClick(e, 0)} />
-        ) : (
-          <BsStar onClick={e => handleStarClick(e, 0)} />
-        )}
-        {clicked[1] ? (
-          <BsStarFill onClick={e => handleStarClick(e, 1)} />
-        ) : (
-          <BsStar onClick={e => handleStarClick(e, 1)} />
-        )}
-        {clicked[2] ? (
-          <BsStarFill onClick={e => handleStarClick(e, 2)} />
-        ) : (
-          <BsStar onClick={e => handleStarClick(e, 2)} />
-        )}
-        {clicked[3] ? (
-          <BsStarFill onClick={e => handleStarClick(e, 3)} />
-        ) : (
-          <BsStar onClick={e => handleStarClick(e, 3)} />
-        )}
-        {clicked[4] ? (
-          <BsStarFill onClick={e => handleStarClick(e, 4)} />
-        ) : (
-          <BsStar onClick={e => handleStarClick(e, 4)} />
-        )}
-      </div>
-    </div>
+    <GradeContainer>
+      <ReviewTitle />
+      <BsStarContainer>
+        <BsStarMargin>
+          {clicked[0] ? (
+            <BsStarFill onClick={e => handleStarClick(e, 0)} />
+          ) : (
+            <BsStar onClick={e => handleStarClick(e, 0)} />
+          )}
+        </BsStarMargin>
+        <BsStarMargin>
+          {clicked[1] ? (
+            <BsStarFill onClick={e => handleStarClick(e, 1)} />
+          ) : (
+            <BsStar onClick={e => handleStarClick(e, 1)} />
+          )}
+        </BsStarMargin>
+        <BsStarMargin>
+          {clicked[2] ? (
+            <BsStarFill onClick={e => handleStarClick(e, 2)} />
+          ) : (
+            <BsStar onClick={e => handleStarClick(e, 2)} />
+          )}
+        </BsStarMargin>
+        <BsStarMargin>
+          {clicked[3] ? (
+            <BsStarFill onClick={e => handleStarClick(e, 3)} />
+          ) : (
+            <BsStar onClick={e => handleStarClick(e, 3)} />
+          )}
+        </BsStarMargin>
+        <BsStarMargin>
+          {clicked[4] ? (
+            <BsStarFill onClick={e => handleStarClick(e, 4)} />
+          ) : (
+            <BsStar onClick={e => handleStarClick(e, 4)} />
+          )}
+        </BsStarMargin>
+      </BsStarContainer>
+    </GradeContainer>
   );
 };
-// const GradeContainer = styled.div`
-//   width: 500px;
-//   margin: 10px;
-// `;
+const GradeContainer = styled.div`
+  width: 500px;
+  margin: 10px;
+`;
 
-// const BsStarContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-// `;
+const BsStarContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
-// const BsStarMargin = styled.div`
-//   margin: 0px 3px;
-// `;
+const BsStarMargin = styled.div`
+  margin: 0px 3px;
+`;
 
 export default Grade;
