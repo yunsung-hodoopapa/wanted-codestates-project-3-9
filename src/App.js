c9e6dcbbc7a13edbb0f21f94db6bd3e6ecc05a37
yunsung-hodoopapa <vappet3477@gamail.com>
2022-03-14 16:42:10 +0900

    feat/Route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DetailView from './pages/DetailView';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Main />} /> */}
+        <Route path="/detailView" element={<DetailView />} />
+        {/* <Route path="/review/id" elemeent={<Review />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/DetailView.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DetailView = () => {
+  return <div></div>;
+};
+
+export default DetailView;
