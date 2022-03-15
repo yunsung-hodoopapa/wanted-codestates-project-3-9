1373b94055880324f7f5fb153b694ff368bd5e74
jihyeon57 <ehwlgus22@gmail.com>
2022-03-15 19:49:43 +0900

    🔥 remove: sort reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,14 +3,12 @@ import { commentReducer } from './comment';
 import { interactionReducer } from './interaction';
 import { postReducer } from './post';
 import { dataReducer } from './dataReducer';
-import { sortReducer } from './sortReducer';
 
 const rootReducer = combineReducers({
   comment: commentReducer,
   interaction: interactionReducer,
   post: postReducer,
   data: dataReducer,
-  sort: sortReducer,
 });
 
 export default rootReducer;
